9a3d7c6a07530c5792d7de1d06e1b73b3a4bb0d6
Kamil Gajowy <kamil.gajowy@gmail.com>
2018-11-17 18:11:29 +0100

    chore: add 2nd chapter to data types

---
+++ 01-typy-tablice.js
@@ -0,0 +1,24 @@
+/** co to jest tablica?
+ * czy napis jest tablicą?
+ * czy tablica musi mieć jeden typ danych?
+ * */
+
+// length, push, pop
+
+// odwolywanie sie do indeksow
+
+// API - split, join
+/**
+ * Ćwiczenie
+ *
+ * używając split i join, odwrócić ciąg znaków.
+ *
+ * metoda ".reverse()" na tablicy odraca kolejność elementów
+ *
+ */
+
+/**
+ * Zadanie projektowe
+ */
+
+// call to przerwa!
