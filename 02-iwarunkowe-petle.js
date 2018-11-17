cab8a66d44278051ce2b6b5da5d707e63c6172e4
Kamil Gajowy <kamil.gajowy@gmail.com>
2018-11-17 19:07:35 +0100

    wip

---
+++ 02-iwarunkowe-petle.js
@@ -0,0 +1,19 @@
+// if, else, wielokrotny blok if
+// do czego to uzywac?
+// mozliwe zagrozenia :)
+
+// sekwencyjne wykonywanie zbioru operacji na kolekcji
+// zakupy :)
+
+// pętla FOR ; continue; break
+
+// pętla while i zamiana pętli for na while :)
+
+// kiedy ktora? przyklad na czytaniu buforu / pliku ; while na napisie
+
+/**
+let napis = 'Warsaw JS'
+while (napis = napis.substr(1)) {
+    console.log(napis.charAt(0))
+}
+ */
--- 02-petle.js
+++
@@ -1,6 +0,0 @@
-// sekwencyjne wykonywanie zbioru operacji na kolekcji
-// zakupy :)
-
-// pętla FOR ; continue; break
-
-// pętla while i zamiana pętli for na while :)
