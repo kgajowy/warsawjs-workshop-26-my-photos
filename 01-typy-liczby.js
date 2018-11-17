be3c1847d55a478a87449604104a65e50b986d2d
Kamil Gajowy <kamil.gajowy@gmail.com>
2018-11-17 18:00:24 +0100

    chore: add guide-by-comments to 1) data types

--- 01-typy-liczby.js
+++ 01-typy-liczby.js
@@ -1,16 +1,26 @@
+/** uruchamiamy przez przeglądarkę */
+
 // deklaracja liczb
 
 // sumowanie
 
+// przypisanie wyniku do zmiennej
 
-let liczba = 2;
-let liczba2 = 3
+// sposoby działań arytmentycznych (* *= ++)
 
-console.log(liczba)
-console.log(liczba + liczba2)
+/** node.js runtime , uruchomienie skryptu z powłoki */
 
-console.log(liczba)
+// deklaracja napisu (ciągu znaków), API;
 
-liczba = liczba + liczba2
+/** quokka */
 
-console.log(liczba)
+// łączenie warunków logicznych
+
+// co jest prawdą wg JS? przykład niepustego napisu, 0 / 1
+
+// niejawna konwersja - liczba na napis,
+
+// dodawanie / odejmowanie
+
+// idea języka - zmienne nie są statycznie typowane - ale mogą!
+// typescript example!
